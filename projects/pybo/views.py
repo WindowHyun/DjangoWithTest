113efa540f59b6688d53417d86aeee4d97d5e482
YyyyyyCH <dusckd4948@naver.com>
2026-04-24 23:48:35 +0900

    Refactor imports in views.py and update binary files
    Run isort --check-only --profile black .
      isort --check-only --profile black .
      shell: /usr/bin/bash -e {0}
      env:
        UV_CACHE_DIR: /home/runner/work/_temp/setup-uv-cache
        pythonLocation: /opt/hostedtoolcache/Python/3.13.13/x64
        PKG_CONFIG_PATH: /opt/hostedtoolcache/Python/3.13.13/x64/lib/pkgconfig
        Python_ROOT_DIR: /opt/hostedtoolcache/Python/3.13.13/x64
        Python2_ROOT_DIR: /opt/hostedtoolcache/Python/3.13.13/x64
        Python3_ROOT_DIR: /opt/hostedtoolcache/Python/3.13.13/x64
        LD_LIBRARY_PATH: /opt/hostedtoolcache/Python/3.13.13/x64/lib
    ERROR: /home/runner/work/DjangoWithTest/DjangoWithTest/projects/config/urls.py Imports are incorrectly sorted and/or formatted.
    ERROR: /home/runner/work/DjangoWithTest/DjangoWithTest/projects/pybo/views.py Imports are incorrectly sorted and/or formatted.
    Skipped 1 files
    Error: Process completed with exit code 1.
    
     이 오류는 Python 코드의 모듈 임포트(import) 순서가 규칙에 맞게 정렬되지 않아서
      발생한 CI(GitHub Actions) 검사 실패 오류입니다.
    
     해결 방법:
      로컬 개발 환경에서 isort를 실행하여 해당 파일들의 임포트 순서를 자동으로 정렬한 후
      다시 커밋하고 푸시하면 해결됩니다.

--- projects/pybo/views.py
+++ projects/pybo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # https://docs.djangoproject.com/en/stable/ref/request-response/#django.http.HttpResponse
 
